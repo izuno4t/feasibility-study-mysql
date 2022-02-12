8ddb354d75480a63fa378e21690187585655e7e3
izuno4t <9846+izuno4t@users.noreply.github.com>
2022-02-12 22:08:28 +0900

    up

--- import.sh
+++ import.sh
@@ -6,10 +6,11 @@ basedir="$(
     pwd
 )"
 
-# rm data-*
-# split -l 1000000 data.csv data-
+rm data-*
+split -l 1000000 data.csv data-
 
 for var in $(ls -1 data-*); do
     time cat ${var} | mysql -h127.0.0.1 -u sample -psample sample -e "LOAD DATA LOCAL INFILE  '/dev/stdin'  INTO TABLE serial_number FIELDS TERMINATED BY ',' ENCLOSED BY '\"';"
     rm ${var}
+    sleep 10
 done
